88b11e5ef431b4bff713bb39fddefcde60d923c3
Peter Kagwe <kagwe@DESKTOP-GI1LUV5>
2024-03-17 17:04:48 +0300

    add agent

--- oware-agent/dapp.py
+++ oware-agent/dapp.py
@@ -1,6 +1,8 @@
 from os import environ
 import logging
 import requests
+from tensorflow.python.keras.models import load_model
+from oware_logic import oware_moves
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
@@ -8,13 +10,29 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
+agent_moves = oware_moves.OwareMoves()
 
-def handle_advance(data):
+model = load_model('model/oware-100.h5')
+
+
+def get_agent_move(model,game_state,player):
+
+    agent_moves.legal_moves_generator(game_state,player)
+
+    agent_move_selected = agent_moves.move_selector(model)
+
+    if len(agent_move_selected) == 3:
+            selected_move,new_board_state,score = agent_move_selected
+    else:
+         pass
+
+
+def handle_advance(data,model):
     logger.info(f"Received advance request data {data}")
     return "accept"
 
 
-def handle_inspect(data):
+def handle_inspect(data,model):
     logger.info(f"Received inspect request data {data}")
     return "accept"
 
@@ -36,4 +54,4 @@ while True:
         rollup_request = response.json()
         data = rollup_request["data"]
         handler = handlers[rollup_request["request_type"]]
-        finish["status"] = handler(rollup_request["data"])
+        finish["status"] = handler(rollup_request["data"],model)
